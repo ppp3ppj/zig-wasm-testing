1a6c35e2a36760bc31c6bbc7dde1aee5014cc1ed
ppp3ppj <ppp.3ppj@gmail.com>
2024-08-29 23:05:11 +0700

    use add from wasm

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+const fs = require('fs');
+const source = fs.readFileSync('./add.wasm');
+
+WebAssembly.instantiate(source, {env: { print: (x) => console.log(x) } })
+.then((result) => {
+    const add = result.instance.exports.add;
+        let a = add(1, 2);
+        console.log(a);
+});
