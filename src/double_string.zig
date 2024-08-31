d6854bd7bc65b006d0d4825a9842b71a405b8bed
ppp3ppj <ppp.3ppj@gmail.com>
2024-08-31 21:30:32 +0700

    add zig double string fn

---
+++ src/double_string.zig
@@ -0,0 +1,12 @@
+const std = @import("std");
+
+export fn doubleString(s: [*]u8, length: usize, capacity: usize) i32 {
+    if (capacity < length * 2) {
+        return -1;
+    }
+
+    const left = s[0..length];
+    const right = s[length .. length * 2];
+    @memcpy(right, left);
+    return @as(i32, @intCast(length)) * 2;
+}
